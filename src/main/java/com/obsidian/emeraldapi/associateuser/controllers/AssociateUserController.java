59f00de8de42394e7a3b06e6ba222547d87c4de6
fersilverio <ssilveriofernando45@gmail.com>
2024-06-30 17:08:14 -0300

    started structure to aid patch method

--- src/main/java/com/obsidian/emeraldapi/associateuser/controllers/AssociateUserController.java
+++ src/main/java/com/obsidian/emeraldapi/associateuser/controllers/AssociateUserController.java
@@ -1,9 +1,11 @@
 package com.obsidian.emeraldapi.associateuser.controllers;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.RecordComponent;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -56,7 +58,7 @@ public class AssociateUserController {
     }
 
     @PatchMapping("/associate-users/{id}")
-    public ResponseEntity<Object> updateAssociateUser(@PathVariable Long id, @RequestBody @Valid UpdateAssociateUserDto dto) {
+    public ResponseEntity<Object> updateAssociateUser(@PathVariable Long id, @RequestBody @Valid UpdateAssociateUserDto dto) throws IllegalArgumentException, IllegalAccessException {
         Optional<AssociateUser> user = associateUserRepository.findById(id);
 
         if (user.isEmpty()) {
@@ -65,8 +67,56 @@ public class AssociateUserController {
         
         var userModel = user.get();
 
-        BeanUtils.copyProperties(dto, userModel);
 
+        UpdateAssociateUserDto obj = new UpdateAssociateUserDto(
+            null, null, "teste", null, null, null, null, null);
+
+        var fields = new ArrayList<Field>();
+        RecordComponent[] components = UpdateAssociateUserDto.class.getRecordComponents();
+        for (var comp : components) {
+            try {
+                Field field = obj.getClass()
+                .getDeclaredField(comp.getName());
+                field.setAccessible(true);
+                fields.add(field);
+
+
+
+                // var a = fields.get(0);
+                // System.out.println(a.getName());
+                // System.out.println(a.get(obj));
+            } catch (NoSuchFieldException e) {
+            }
+        }
+
+        for (var i = 0; i < fields.size(); i++) {
+            var a = fields.get(i);
+            System.out.println(a.getName());
+            System.out.println(a.get(obj));
+
+        }
+        
+        // var a = fields.get(0);
+        //         System.out.println(a.getName());
+        //         System.out.println(a.get(obj));
+
+        // var fields = new ArrayList<Field>();
+        // for (var field: UpdateAssociateUserDto.class.getDeclaredFields()) {
+        //     field.setAccessible(true);
+        //     fields.add(field);
+        // }
+
+        // for (var elem: fields) {
+        //     UpdateAssociateUserDto obj = new UpdateAssociateUserDto();
+        //     System.out.println(elem.getName());
+        // }
+        
+        // fields.get()
+
+        // System.out.println(fields);
+        
+        //BeanUtils.copyProperties(dto, userModel);
+        
         return ResponseEntity.status(HttpStatus.OK).body(associateUserRepository.save(userModel));
     }
 
